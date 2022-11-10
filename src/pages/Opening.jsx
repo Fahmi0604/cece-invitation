d902ca8747c365fda12682a63fc68b010cc38a1a
fahmi0604 <fahmifirmansyah917@gmail.com>
2022-11-10 22:55:01 +0700

    update

--- src/pages/Opening.jsx
+++ src/pages/Opening.jsx
@@ -22,7 +22,7 @@ export default function ({ setisOpen}) {
             <h2 className='relative text-white text-5xl md:text-9xl font-[Alice] px-12 pb-5 rounded-full'>Yenny & Rio</h2>
             <p className='text-2xl md:text-5xl text-white font-[Alegreya] mb-8'>Kepada Bapak/Ibu/Saudara/i</p>
             <p className='text-3xl md:text-7xl text-white font-[Alegreya] mb-8'>User1</p>
-            <p className='text-xl md:text-3xl text-white font-[Alegreya] mb-8'>Kami mengundang Anda untuk menghadiri acara pernikahan kami.</p>
+            <p className='text-xl md:text-3xl text-white font-[Alegreya] mb-8 mx-4'>Kami mengundang Anda untuk menghadiri acara pernikahan kami.</p>
             <button onClick={() => setclick(true)} className='bg-[#272726] text-white font-[Alegreya] text-2xl px-4 py-2 rounded-full'>Buka Undangan</button>
         </div>
     </div>
