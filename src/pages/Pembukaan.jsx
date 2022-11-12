753c66cf804be3b2a89de3e6dfb6a3df5935c04c
nuruslaily <nuruslaily88@gmail.com>
2022-11-12 20:20:22 +0700

    update nama mempelai

--- src/pages/Pembukaan.jsx
+++ src/pages/Pembukaan.jsx
@@ -13,13 +13,15 @@ export default function Pembukaan() {
             <p className='text-xl md:text-2xl font-[Alegreya]'>Putra dari Bapak Rudi H.Kurniawan Salim dan Ibu </p>
         </div> */}
         <div className=" w-[90%] md:w-full mx-[5%] md:mx-0 bg-custom-white-md text-center py-4 text-white rounded-2xl md:rounded-none">
-            <h2 className='text-4xl md:text-5xl font-[Alice] mb-8'>Pembukaan</h2>
-
-            <p className='text-3xl md:text-4xl font-[Alegreya]'>Robertus Romario</p>
+            <h2 className='text-4xl md:text-5xl font-[Alice] mb-8'>Groom & Bride</h2>
+            
+            <p className='text-3xl md:text-4xl font-[Alegreya]'>Robertus Romario Harvey Kurniawan</p>
             <p className='text-xl md:text-2xl font-[Alegreya] mb-4'>Putra dari Rudi Kurniawan Salim dan Susanti</p>
             <p className='text-5xl font-[Alegreya] mb-4'>&</p>
-            <p className='text-3xl md:text-4xl font-[Alegreya]'>Yenny Candrawati Salim</p>
+            <p className='text-3xl md:text-4xl font-[Alegreya]'>Yenny Chandrawati Salim</p>
             <p className='text-xl md:text-2xl font-[Alegreya]'>Putri dari Pendi Salim dan Indrajayani Soetrisno</p>
+            
+            <h4 className='text-2xl mt-8 md:text-2xl font-[Alegreya]'>#ourstoRY</h4>
         </div>
     </div>
   )
